49f93355852f34cdd5201851735d246299f9bb9b
ejamdar05 <jamdareshaq@gmail.com>
2024-05-17 19:24:49 -0700

    reduced actio space to number of shortest paths

--- netenv.py
+++ netenv.py
@@ -7,21 +7,35 @@ import random
 class NetworkEnvironment(gym.Env):
     def __init__(self):
         super().__init__()
+        # Read the network file
         self.graph = nx.read_gml('nsfnet.gml')
-        self.num_slots = 10 
+        # Max utilization per slot
+        self.num_slots = 10
+        # Total edges/links
         self.total_links = len(self.graph.edges())
+
+        # State of links
         self.link_states = {}
         for edge in self.graph.edges():
+            # Initialize the state of all edge utilization to 0
             self.link_states[edge] = np.zeros(self.num_slots, dtype=int)
-        
-        self.action_space = spaces.Discrete(self.num_slots * self.total_links * 3)
-        self.observation_space = spaces.Box(low=0, high=self.num_slots, shape=(self.total_links * self.num_slots,), dtype=int)
 
-        self.isRandom = False
+        self.isRandom = True
+        #generate list of traffic requests (100)
         self.traffic_requests = self.generate_traffic_requests()
-        self.current_request_index = 0 
+        #generate the shortest paths from traffic reqs
+        self.paths = self.generate_paths()
 
-        
+        self.action_space = spaces.Discrete(len(self.paths))
+        print(len(self.paths))
+        self.observation_space = spaces.Box(low=0, high=20, shape=(self.total_links * self.num_slots,), dtype=int)
+        self.current_request_index = 0
+
+    def generate_paths(self):
+        paths = []
+        for s, d, _, in self.traffic_requests:
+            paths = paths + list(nx.shortest_simple_paths(self.graph, source=s, target=d))
+        return paths
 
     def generate_traffic_requests(self):
         requests = []
@@ -44,29 +58,52 @@ class NetworkEnvironment(gym.Env):
         return self.get_observation(), {}
 
     def step(self, action):
-        link_index = (action // 3) // self.num_slots
-        slot_index = (action // 3) % self.num_slots
-        action_type = action % 3
+        path = self.paths[action]
         reward = 0
+        req = self.traffic_requests[self.current_request_index]
+        util = self.utilzle_link(path, req[2])
 
-        link = list(self.graph.edges())[link_index]
-        if action_type == 0:
-            if self.link_states[link][slot_index] < self.num_slots:
-                self.link_states[link][slot_index] += 1
-                reward += 1
-            else:
-                reward -= 1
-        elif action_type == 1:
-            if self.link_states[link][slot_index] > 0:
-                self.link_states[link][slot_index] -= 1
-                reward = 0.5
-            else:
-                reward -= 0.5
+        if util:
+            reward += 1
+            print(f"Reward for {path} {req}")
+        else:
+            reward -= 1
+            print(f"Failed at {path} {req}. No reward.")
+        
+        self.decreaseHoldTime()
+        
+
         self.current_request_index += 1
         done = self.current_request_index >= len(self.traffic_requests)
         info = {}
         return self.get_observation(), reward, done, False, info
 
+    def decreaseHoldTime(self):
+        for edge in self.link_states:
+            for slot in range(self.num_slots):
+                if self.link_states[edge][slot] > 0:
+                    self.link_states[edge][slot] -= 1
+        
+    
+    def utilzle_link(self, path, ht):
+        for slot in range(self.num_slots):
+            allocated = True
+            for i in range (len(path) - 1):
+                edge = (path[i], path[i+1])
+                if edge not in self.link_states:
+                    edge = (path[i+1], path[i])
+                if self.link_states[edge][slot] != 0:
+                    allocated = False
+                    break
+            
+            if allocated:
+                for j in range(len(path) - 1):
+                    edge = (path[j], path[j+1])
+                    if edge not in self.link_states:
+                        edge = (path[j+1], path[j])
+                    self.link_states[edge][slot] = ht
+                return True
+        return False 
 
     def get_observation(self):
         obs = []
@@ -74,5 +111,4 @@ class NetworkEnvironment(gym.Env):
             for state in states:
                 obs.append(state)
         return np.array(obs)
-
 
